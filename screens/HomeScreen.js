c8c30a2fce1606cd920b231b7576874b3ef7de54
Pravin Kadam <pravinkadam21998@gmail.com>
2024-04-09 11:37:03 +0530

    initial setup and trending page

---
+++ screens/HomeScreen.js
@@ -0,0 +1,51 @@
+import {
+  View,
+  Text,
+  Platform,
+  TouchableOpacity,
+  ScrollView,
+} from "react-native";
+
+import { useState } from "react";
+import React from "react";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StatusBar } from "expo-status-bar";
+import {
+  Bars3CenterLeftIcon,
+  MagnifyingGlassIcon,
+} from "react-native-heroicons/outline";
+import TrendingMovies from "../components/trendingMovies";
+const ios = Platform.OS == "ios";
+
+export default function HomeScreen() {
+  const [trending, setTrending] = useState([1, 2, 3]);
+
+  return (
+    <View className="flex-1 bg-neutral-800">
+      {/* search bar and logo */}
+      <SafeAreaView className={ios ? "-mb-2" : "mb-3"}>
+        <StatusBar style="light" />
+        <View className="flex-row justify-between item-centre mx-4 my-1">
+          <Bars3CenterLeftIcon size="35" strokeWidth={2} color="white" />
+          <Text className="text-white text-3xl font-bold">
+            <Text style={{ color: "#eab308" }}>M</Text>ovies
+          </Text>
+          <TouchableOpacity>
+            <MagnifyingGlassIcon
+              size="35"
+              strokeWidth={2}
+              color="white"
+            ></MagnifyingGlassIcon>
+          </TouchableOpacity>
+        </View>
+      </SafeAreaView>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ padding: 10 }}
+      >
+        {/* trending movie carousal */}
+        <TrendingMovies data={trending}></TrendingMovies>
+      </ScrollView>
+    </View>
+  );
+}
